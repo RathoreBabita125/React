19df6882d3b4e7b563717c5f879773529e64f603
Babita <babitarathor100@gmail.com>
2025-07-08 01:43:50 +0530

    Practice

--- Practice/Project-7/Theme-Changer/src/App.jsx
+++ Practice/Project-7/Theme-Changer/src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useEffect } from 'react'
+import ThemeBtn from './Components/ThemeBTN'
+import Card from './Components/Card'
+import { ThemeProvider } from './Context/ThemeContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const[themeMode, setThemeMode]=useState('light')
+
+  const lightTheme=()=>{
+    setThemeMode('light')
+  }
+
+  const darkTheme=()=>{
+    setThemeMode('dark')
+  }
+
+  useEffect(()=>{
+        document.querySelector('html').classList.remove('light','dark')
+        document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className='w-full '>
+        <div className='w-1/3 items-center m-auto'>
+          <h1 className='bg-gray-900 font-bold text-amber-100 mb-[12px]'>Theme Changer</h1>
+          <ThemeBtn/>
+          <Card/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </ThemeProvider>
+    
   )
 }
 
